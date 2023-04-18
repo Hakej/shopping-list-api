71791a74995a022b20d55f15ae5e9826de1daf11
Hakej <michaluszak97@gmail.com>
2023-04-18 14:41:55 +0200

    Fix CORS & add CORS whitelist

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { CurrentDateLogger } from './Logger/CurrentDateLogger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  const currentDateLogger = new CurrentDateLogger(60 * 1000);
+  const whitelist = process.env.CORS_WHITELIST.split(' ');
+  app.enableCors({
+    origin: function (origin, callback) {
+      if (!origin || whitelist.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+  });
   await app.listen(process.env.PORT || 5001);
 }
 bootstrap();
